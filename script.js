8d28c447834c85668b4a18d1bedf52e61bce4219
Kenechukwu Akupue <kenechukwuakupueofficial@gmail.com>
2025-02-18 00:59:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function showAbout() {
+    const showabout = document.querySelector('.about-us');
+    showabout.style.display = 'flex';
+}
+
+function hideAbout() {
+    const hideabout = document.querySelector('.about-us');
+    hideabout.style.display = 'none';
+}
+
+function showForm() {
+    const showform = document.querySelector('.order-form');
+    showform.style.display = 'flex';
+}
+
+function hideForm() {
+    const hideform = document.querySelector('.order-form');
+    hideform.style.display = 'none';
+}
+
+function showFormQ() {
+    const showform = document.querySelector('.questionnaire-form');
+    showform.style.display = 'flex';
+}
+
+function hideFormQ() {
+    const hideform = document.querySelector('.questionnaire-form');
+    hideform.style.display = 'none';
+}
+
+function showBar() {
+    const showbar = document.querySelector('.sidebar');
+    showbar.style.display = 'flex';
+}
+function hideBar() {
+    const hidebar = document.querySelector('.sidebar');
+    hidebar.style.display = 'none';
+}
